9110174803efd4e67e22745c494ddb3da370dde5
mirian <roxsis18@gmail.com>
2016-03-05 17:20:11 -0500

    iniciando proyecto

---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="public/css/bootstrap.css">
+</head>
+<body>
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">ESCUELA DE CANTOS</a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse navbar-ex1-collapse">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#">Link</a></li>
+				<li><a href="#">Link</a></li>
+			</ul>
+			<form class="navbar-form navbar-left" role="search">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Search">
+				</div>
+				<button type="submit" class="btn btn-default">Submit</button>
+			</form>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">Link</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Action</a></li>
+						<li><a href="#">Another action</a></li>
+						<li><a href="#">Something else here</a></li>
+						<li><a href="#">Separated link</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</div>
+</nav>
+</div>
+
+
+
+
+
+
+
+<div id="carousel-id" class="carousel slide" data-ride="carousel">
+	<ol class="carousel-indicators">
+		<li data-target="#carousel-id" data-slide-to="0" class=""></li>
+		<li data-target="#carousel-id" data-slide-to="1" class=""></li>
+		<li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+	</ol>
+	<div class="carousel-inner">
+		<div class="item">
+			<img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide" src="public/slider/i.jpg">
+			<div class="container">
+				<div class="carousel-caption">
+					<h1>Example headline.</h1>
+					<p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+					<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+				</div>
+			</div>
+		</div>
+		<h1></h1>
+		<div class="item">
+			<img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" alt="Second slide" src="public/slider/l.jpg">
+			<div class="container">
+				<div class="carousel-caption">
+					<h1>Another example headline.</h1>
+					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+					<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="item active">
+			<img data-src="holder.js/900x500/auto/#555:#5a5a5a/text:Third slide" alt="Third slide" src="public/slider/ñ.PNG">
+			<div class="container">
+				<div class="carousel-caption">
+					<h1>One more for good measure.</h1>
+					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+					<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+	<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+</div>
+
+	
+<div class="container">
+<h1>CURSOS PARA CANTO</h1>
+	
+	  
+	<div class="row">
+		<div class="col-md-6">
+			<div class="mi-video">
+				<video autobuffer autoloop loop controls>
+					<source src="public/video/Curso de canto Gratis 2-12.mp4">
+				</video>
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="mi-video">
+				<video autobuffer autoloop loop controls>
+					<source src="public/video/Curso de canto Gratis 3-12.mp4">
+				</video>
+			</div>
+		</div>
+		<div class="row">
+		<div class="col-md-6">
+			<div class="mi-video">
+				<video autobuffer autoloop loop controls>
+					<source src="public/video/Curso de canto Gratis 2-12.mp4">
+				</video>
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="mi-video">
+				<video autobuffer autoloop loop controls>
+					<source src="public/video/Curso de canto Gratis 3-12.mp4">
+				</video>
+			</div>
+		</div>
+		<h1>ALFIN YA ACABE</h1>
+         <h2>YUPI</h2>
+
+
+
+
+
+</div>
+
+
+<script src="public/js/jquery-1.12.1.js"></script>
+<script src="public/js/bootstrap.js"></script>
+</body>
+
+
+<div class="<col-md-4>">col</div>
+<div class="col-md-4">col</div>
+<div class="col-md-4>col</div>
